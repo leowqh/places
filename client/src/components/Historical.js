e45c980e19262ba33bc85b1c807ea401020c6b8e
student <tohminh99@yahoo.com>
2018-07-10 12:55:40 +0800

    added form, historical, and main

---
+++ client/src/components/Historical.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Historical extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Historical works</h1>
+      </div>
+    );
+  }
+}
+
+export default Historical;
---
+++ client/src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Main works</h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
