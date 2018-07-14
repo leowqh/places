dd7ebfffed8af97ad6f512dbf5b6e95bb610d3e5
student <tohminh99@yahoo.com>
2018-07-14 14:02:33 +0800

    heroku deploy

--- app.js
+++ app.js
@@ -17,6 +17,13 @@ hbs.registerPartials(__dirname + '/views/partials');
 const PLACES_API_KEY = 'AIzaSyAydUEdzB72BZQ-L2S7FlH1FcVlpRe3cdU';
 var filteredResults;
 
+if (process.env.NODE_ENV === 'production') {
+  server.use(express.static('client/build'));
+  server.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 hbs.registerHelper('list', (items,options) => {
   items = filteredResults;
   var out = "<tr><th>Name</th><th>Address</th><th>Photo</th></tr>";
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import qs from 'qs';
+import Result from './Result';
 
 class Form extends Component {
+  constructor() {
+    super();
+    this.state= {
+      addr: '',
+      name: '',
+      result: []
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({[e.target.name]: e.target.value});
+  }
 
   onSubmit(e) {
     e.preventDefault();
 
-    axios.post('http://localhost:5000/getplaces', newQuery)
-    .then((result) => {
+    const newQuerry = {
+      address: this.state.addr,
+      name: this.state.name,
+      placetype: this.state.placetype
+    };
 
+    axios.post('/getplaces', qs.stringify(newQuerry))
+    .then((result) => {
+      this.setState({result: result.data});
     })
     .catch((err) => {
       console.log(err);
@@ -27,14 +51,36 @@ class Form extends Component {
             <form onSubmit={this.onSubmit}>
               <div className="form-group">
                 <label htmlFor="addr">Enter address:</label>
-                <input id="addr" name="address" className="form-control" type="text" placeholder="Enter address..."/>
+                <input
+                  id="addr"
+                  name="addr"
+                  className="form-control"
+                  type="text"
+                  placeholder="Enter address..."
+                  value={this.state.addr}
+                  onChange={this.onChange}
+                />
                 <p></p>
                 <label htmlFor="name">Enter name:</label>
-                <input id="name" name="name" className="form-control" type="text" placeholder="Enter name..."/>
+                <input
+                  id="name"
+                  name="name"
+                  className="form-control"
+                  type="text"
+                  placeholder="Enter name..."
+                  value={this.state.name}
+                  onChange={this.onChange}
+                />
                 <p></p>
                 <label htmlFor="places-type">Select type of place:</label>
                 <p></p>
-                  <select id="places-type" name="placetype" className="form-control">
+                  <select
+                    id="places-type"
+                    name="placetype"
+                    className="form-control"
+                    value={this.state.placetype}
+                    onChange={this.onChange}
+                  >
                   <option value="accounting">accounting</option>
                   <option value="airport">airport</option>
                   <option value="amusement_park">amusement_park</option>
@@ -133,7 +179,7 @@ class Form extends Component {
             </form>
           </div>
         </div>
-
+        <Result list={this.state.result} />
       </div>
     );
   }
